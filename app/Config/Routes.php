dcf0d8732c8bdee161b33cb80b1333d545f72cf5
madhuri <madhuri.k@kitintellect.com>
2023-05-16 12:19:28 +0530

    school route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -84,6 +84,13 @@ $routes->post("login", "Login::login");
 //Specialization
 $routes->get("specialization/listAll","Specialization::index"); 
 
+// School
+$routes->get("school/listAll","School::index");
+$routes->post('school/create','School::create');
+$routes->patch('school/(:num)','School::update/$1');
+$routes->delete("school/delete/:num","School::delete/$1");
+$routes->get('school/(:num)','School::show/$1');
+
 
 
 
